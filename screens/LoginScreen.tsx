1cc33b52a1d57577ba7cbbdf10fa0e928709251a
AndersonBaezP <andersonbaez1990@gmail.com>
2024-07-03 19:09:06 -0500

    TEXT_IMPUT_LOGIN

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -3,12 +3,39 @@ import React from 'react'
 
 export default function LoginScreen() {
     return (
-        <View style={styles.container}>
-            <Text>login</Text>
-        </View>
-    )
-}
+      <ImageBackground 
+      source={{uri:"https://images.pexels.com/photos/5935788/pexels-photo-5935788.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}}
+      style={styles.container}
+      >
 
+        <Text style={{ color:'#0df2c9', fontSize:60, fontWeight:'bold', marginBottom:30}}>Log in</Text>
+    
+        <TextInput
+          placeholder='User or Email'
+          placeholderTextColor={'white'}
+          style={styles.input}
+        />
+
+        <TextInput
+          placeholder='Password'
+          placeholderTextColor={'white'}
+          secureTextEntry={true}
+          style={styles.input}
+          />
+
+        <TouchableOpacity>
+          <Text style={{color:'#3169E9'}}>Did you forget your password?</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} 
+                          onPress={()=> navigation.navigate('BottonTab')}>
+          <Text style={styles.buttonText}>Log in</Text>
+        </TouchableOpacity>
+
+    </ImageBackground>
+  )
+}
+ 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
